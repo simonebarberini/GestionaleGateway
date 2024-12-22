6856012d7b179da32bf5b79006f30fcb629f6177
simonebarberini <simonebarberini02@gmail.com>
2024-12-22 14:28:50 +0100

    initial commit + modifica port application.properties

---
+++ src/main/java/com/gestionale/apiGateway/GesionaleGateway/GesionaleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gestionale.apiGateway.GesionaleGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesionaleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesionaleGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionale/apiGateway/GesionaleGateway/GesionaleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionale.apiGateway.GesionaleGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesionaleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
